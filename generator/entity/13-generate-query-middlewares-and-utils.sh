61590ebaf2aee19dcdb1590a828adb0802a76f0a
Franco Toledo <fr4nc0t2@gmail.com>
2025-07-24 00:42:55 -0300

    Se ajusta el cli y nombres de directorios y package.json

---
+++ generator/entity/13-generate-query-middlewares-and-utils.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# hexagonizer/entity/13-generate-query-middlewares.sh
+# shellcheck disable=SC1091
+
+source ./hexagonizer/common/generate-query-middlewares.sh
+source ./hexagonizer/common/generate-query-utils.sh
--- hexagon/project/07-generate-query-utils.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# hexagon/project/07-generate-query-utils.sh
-# shellcheck disable=SC1091
-
-source ./hexagon/common/generate-query-utils.sh
